9ae8e845efb78c086e4800fb91c4d1732b836ffc
Travis Petersen <travis.e.petersen@gmail.com>
2017-12-17 00:50:34 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -12,7 +12,7 @@ class Application
       resp.write item.price
       resp.status = 200
     else
-      resp.write "Route not found."
+      resp.write "Item not found."
       resp.status = 404
     end
 
