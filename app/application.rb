a0cbf168dcda9027670c0eb88efb0c8b461c515a
Travis Petersen <travis.e.petersen@gmail.com>
2017-12-17 00:57:32 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -7,7 +7,7 @@ class Application
 
     item_name = req.path.split(/items/).last
     item = @@items.find{ |i| i.name == item_name}
-  
+
     if item
       resp.write item.price
       resp.status = 200
