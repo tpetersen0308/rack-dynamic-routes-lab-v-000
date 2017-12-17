167213c11bef7de8cb91b81d367ba4ed36072179
Travis Petersen <travis.e.petersen@gmail.com>
2017-12-17 00:53:04 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -11,7 +11,7 @@ class Application
     if @@items.include?(item)
       resp.write item.price
       resp.status = 200
-    elsif resp.path.match(/items/)
+    elsif req.path.match(/items/)
       resp.write "Item not found"
       resp.status = 400
     else
