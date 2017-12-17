0899adcfffd9a8c05e8dd2ba50a8841c60a37b75
Travis Petersen <travis.e.petersen@gmail.com>
2017-12-17 00:54:00 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,3 +1,5 @@
+require "pry"
+
 class Application
   @@items = []
 
@@ -7,7 +9,7 @@ class Application
 
     item_name = req.path.split(/items/).last
     item = @@items.find{ |i| i.name == item_name}
-
+    binding.pry
     if @@items.include?(item)
       resp.write item.price
       resp.status = 200
